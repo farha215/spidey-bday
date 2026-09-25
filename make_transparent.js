454358c38de86536f73ece7f2d30822c984e2dae
farha <farhapk30@gmail.com>
2026-09-25 06:00:33 +0530

    Clean up unused folders (songs/, root assets/, temp scripts)

--- make_transparent.js
+++
@@ -1,2 +0,0 @@
-const fs = require('fs');
-// Very simple script to remove white background from PNG if it's actually an uncompressed or simple format, but wait, we need a library for JPEG/PNG parsing.
